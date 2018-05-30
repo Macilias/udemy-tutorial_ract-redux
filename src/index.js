72628ca71407c0130498a50610d7d3c990d569ab
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2018-05-30 09:50:41 +0200

    init 1: first lesson

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './compontents/search_bar';
 import VideoList from './compontents/video_list';
-const KEY = 'AIzaSyCC4vZAYbPMxItoFNGVI1NZw5S-XcIO6uI';
+import VideoDetail from './compontents/video_detail';
+// Failed to load https://googleads.g.doubleclick.net/pagead/id: Redirect from
+// 'https://googleads.g.doubleclick.net/pagead/id' to 'data:text;charset=utf-8,'
+// has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present
+// on the requested resource. Origin 'https://www.youtube.com' is therefore not allowed access.
+
+const API_KEY = 'AIzaSyCC4vZAYbPMxItoFNGVI1NZw5S-XcIO6uI';
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +17,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({ videos });
     });
   }
@@ -20,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );

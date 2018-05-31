070b5824d145ce3bfb30ce5dc97d79e57c024f01
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2018-05-31 09:32:21 +0200

    fixed not loading video, className had a typo, thankx Bobby!

--- src/compontents/video_detail.js
+++ src/compontents/video_detail.js
@@ -11,8 +11,8 @@ const VideoDetail = ({video}) => {
 
 	return (
 		<div className="video-detail col-md-8">
-			<div className="embed-responsive embed-responsive-16y9">
-				<iframe className="embed-responsive-item" src={url}></iframe>
+			<div className="embed-responsive embed-responsive-16by9">
+				<iframe className="embed-responsive-item" src={url}/>
 			</div>
 			<div className="details">
 				<div>{video.snippet.title}</div>

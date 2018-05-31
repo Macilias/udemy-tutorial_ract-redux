b00f9e70cf6cd8834ea6cab0d360eca7dff40311
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2018-05-31 13:52:59 +0200

    Section 2: Final state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './compontents/search_bar';
 import VideoList from './compontents/video_list';
 import VideoDetail from './compontents/video_detail';
-// Failed to load https://googleads.g.doubleclick.net/pagead/id: Redirect from
-// 'https://googleads.g.doubleclick.net/pagead/id' to 'data:text;charset=utf-8,'
-// has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present
-// on the requested resource. Origin 'https://www.youtube.com' is therefore not allowed access.
-
 const API_KEY = 'AIzaSyCC4vZAYbPMxItoFNGVI1NZw5S-XcIO6uI';
 
 class App extends Component {
@@ -33,10 +29,12 @@ class App extends Component {
   }
 
   render() {
+  	const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
     return (
       <div>
         <SearchBar
-					onSearchTermChange={term => this.videoSearch(term)}
+					onSearchTermChange={videoSearch}
 				/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
